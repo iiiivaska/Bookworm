8405a9310cea6295e74dd782cabc8c9b39bbda55
iiiivaska <iiiivaska@yandex.ru>
2021-11-10 15:14:37 +0300

    another persistenceController

---
+++ Bookworm/Persistence.swift
@@ -0,0 +1,25 @@
+//
+//  PersistenceController.swift
+//  Bookworm
+//
+//  Created by Василий Буланов on 11/10/21.
+//
+
+import Foundation
+import CoreData
+
+struct PersistenceController {
+    static let shared = PersistenceController()
+
+    let container: NSPersistentContainer
+
+    init() {
+        container = NSPersistentContainer(name: "Bookworm")
+
+        container.loadPersistentStores{ (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolve Error: \(error)")
+            }
+        }
+    }
+}
